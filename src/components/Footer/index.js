3324aa9ebfd68de0d2206ef11d98cf850d3ba26c
pedrosvalentim <67519355+pedrosvalentim@users.noreply.github.com>
2020-07-28 14:23:26 -0300

    Setup inicial do projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src='https://fontmeme.com/permalink/200728/a902109955f19fc1c4d59731b9ca9c2a.png' alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
